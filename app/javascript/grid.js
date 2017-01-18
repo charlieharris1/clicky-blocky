2b3f11f11be35fa757892a45a1c0bca25f458369
Charlie Harris <ceharris92@gmail.com>
2017-01-18 22:17:59 +0000

    Get all adjacent elems with the same colour

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -35,14 +35,14 @@ export class BlockGrid {
             el.appendChild(colEl);
 
             for (let y = MAX_Y - 1; y >= 0; y--) {
-                let block = this.grid[x][y],
+                let block = this.grid[x][y], // TODO: this is how we can reassign the blocks
                     id = `block_${x}x${y}`,
                     blockEl = document.createElement('div');
 
                 blockEl.id = id;
                 blockEl.className = 'block';
                 blockEl.style.background = block.colour;
-                blockEl.addEventListener('click', (evt) => this.blockClicked(evt, block));
+                blockEl.addEventListener('click', (evt) => this.blockClicked(evt, block, id));
                 colEl.appendChild(blockEl);
             }
         }
@@ -50,8 +50,34 @@ export class BlockGrid {
         return this;
     }
 
-    blockClicked (e, block) {
-        console.log(e, block);
+    blockClicked (e, block, id) {
+        // document.getElementById(id).setAttribute('id', 'charlie');
+        // is it possible to set element id based on its coordinates
+        // for each column go through and update their coordinates
+
+        // 1. Get the colors of the surrounding blocks.
+        let ids = [];
+
+        function getIdsOfAdjacentCellsWithTheSameColour(elemId) {
+            var idOfBlockAbove = 'block_' + (block.x) + 'x' + (block.y + -1 );
+            var idOfBlockBelow = 'block_' + (block.x) + 'x' + (block.y + 1 );
+            var idOfBlockLeft = 'block_' + (block.x - 1) + 'x' + (block.y);
+            var idOfBlockRight = 'block_' + (block.x + 1) + 'x' + (block.y);
+
+            var blockColour = document.getElementById(elemId).style.background;
+            var adjacentBlocks = [idOfBlockAbove, idOfBlockBelow, idOfBlockRight, idOfBlockLeft];
+
+            return adjacentBlocks.reduce(function(accumulator, id) {
+                var adjacentBlockColour = document.getElementById(id).style.background;
+                if (adjacentBlockColour === blockColour) {
+                    return [].concat(accumulator, id)
+                }
+                return accumulator;
+            }, []);
+        }
+
+        console.log('IDs ----', getIdsOfAdjacentCellsWithTheSameColour(id)); // KEEP GOING TILL THERE ARE NONE
+        console.log(e, block, id);
     }
 }
 
