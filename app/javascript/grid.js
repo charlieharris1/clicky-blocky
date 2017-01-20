79625f058576c3e8a07fa012df8aa22b2c4c20ff
Charlie Harris <ceharris92@gmail.com>
2017-01-20 23:39:19 +0000

    Add base airbnb eslint rule to grid.js file

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -1,91 +1,92 @@
+/* eslint-env browser */
 export const COLOURS = ['red', 'green', 'blue', 'yellow'];
 const MAX_X = 10;
 const MAX_Y = 10;
 
 export class Block {
-    constructor (x, y) {
-        this.x = x;
-        this.y = y;
-        this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
+  }
 }
 
 export class BlockGrid {
-    constructor () {
-        this.grid = [];
+  constructor() {
+    this.grid = [];
 
-        for (let x = 0; x < MAX_X; x++) {
-            let col = [];
-            for (let y = 0; y < MAX_Y; y++) {
-                col.push(new Block(x, y));
-            }
+    for (let x = 0; x < MAX_X; x += 1) {
+      const col = [];
+      for (let y = 0; y < MAX_Y; y += 1) {
+        col.push(new Block(x, y));
+      }
 
-            this.grid.push(col);
-        }
-
-        return this;
+      this.grid.push(col);
     }
 
-    render (el = document.querySelector('#gridEl')) {
-        for (let x = 0; x < MAX_X; x++) {
-            let id = 'col_' + x;
-            let colEl = document.createElement('div');
-            colEl.className = 'col';
-            colEl.id = id;
-            el.appendChild(colEl);
+    return this;
+  }
 
-            for (let y = MAX_Y - 1; y >= 0; y--) {
-                let block = this.grid[x][y],
-                    id = `block_${x}x${y}`,
-                    blockEl = document.createElement('div');
+  render(el = document.querySelector('#gridEl')) {
+    for (let x = 0; x < MAX_X; x += 1) {
+      const colId = `col_${x}`;
+      const colEl = document.createElement('div');
+      colEl.className = 'col';
+      colEl.id = colId;
+      el.appendChild(colEl);
 
-                blockEl.id = id;
-                blockEl.className = 'block';
-                blockEl.style.background = block.colour;
-                blockEl.addEventListener('click', (evt) => this.blockClicked(id));
-                colEl.appendChild(blockEl);
-            }
-        }
+      for (let y = MAX_Y - 1; y >= 0; y -= 1) {
+        const block = this.grid[x][y];
+        const blockId = `block_${x}x${y}`;
+        const blockEl = document.createElement('div');
 
-        return this;
+        blockEl.id = blockId;
+        blockEl.className = 'block';
+        blockEl.style.background = block.colour;
+        blockEl.addEventListener('click', () => this.blockClicked(blockId));
+        colEl.appendChild(blockEl);
+      }
     }
 
-    blockClicked (targetBlockId) {
-        let blocksChecked = [];
-        const targetBlockColour = document.getElementById(targetBlockId).style.background;
+    return this;
+  }
 
-        function connectedBlocksWithSameColour(blockId) {
-            const blockHasBeenChecked = blocksChecked.indexOf(blockId) >= 0;
+  blockClicked(targetBlockId) {
+    const blocksChecked = [];
+    const targetBlockColour = document.getElementById(targetBlockId).style.background;
 
-            if (blockHasBeenChecked) return null;
-            blocksChecked.push(blockId);
+    function connectedBlocksWithSameColour(blockId) {
+      const blockHasBeenChecked = blocksChecked.indexOf(blockId) >= 0;
 
-            const block = document.getElementById(blockId).getBoundingClientRect();
+      if (blockHasBeenChecked) return null;
+      blocksChecked.push(blockId);
 
-            const middleVertical = block.top + (block.height / 2);
-            const middleHorizontal = block.left + (block.width / 2);
+      const block = document.getElementById(blockId).getBoundingClientRect();
 
-            const blockBelow = document.elementFromPoint(middleHorizontal, block.bottom + 1);
-            const blockAbove = document.elementFromPoint(middleHorizontal, block.top - 1);
-            const blockLeft = document.elementFromPoint(block.left -1 , middleVertical);
-            const blockRight = document.elementFromPoint(block.right + 1, middleVertical);
+      const middleVertical = block.top + (block.height / 2);
+      const middleHorizontal = block.left + (block.width / 2);
 
-            const adjacentBlocks = [blockBelow, blockAbove, blockLeft, blockRight];
+      const blockBelow = document.elementFromPoint(middleHorizontal, block.bottom + 1);
+      const blockAbove = document.elementFromPoint(middleHorizontal, block.top - 1);
+      const blockLeft = document.elementFromPoint(block.left - 1, middleVertical);
+      const blockRight = document.elementFromPoint(block.right + 1, middleVertical);
 
-            return adjacentBlocks.reduce((accumulator, adjacentBlock)  => {
-                if (!adjacentBlock) return accumulator;
+      const adjacentBlocks = [blockBelow, blockAbove, blockLeft, blockRight];
 
-                if (adjacentBlock.style.background === targetBlockColour) {
-                    return [].concat(accumulator, connectedBlocksWithSameColour(adjacentBlock.id));
-                }
+      return adjacentBlocks.reduce((accumulator, adjacentBlock) => {
+        if (!adjacentBlock) return accumulator;
 
-                return accumulator;
-            }, [ blockId ]);
+        if (adjacentBlock.style.background === targetBlockColour) {
+          return [].concat(accumulator, connectedBlocksWithSameColour(adjacentBlock.id));
         }
 
-        connectedBlocksWithSameColour(targetBlockId)
-          .forEach((id) => (id ? document.getElementById(id).remove() : null))
+        return accumulator;
+      }, [blockId]);
     }
+
+    connectedBlocksWithSameColour(targetBlockId)
+          .forEach(id => (id ? document.getElementById(id).remove() : null));
+  }
 }
 
 window.addEventListener('DOMContentLoaded', () => new BlockGrid().render());
