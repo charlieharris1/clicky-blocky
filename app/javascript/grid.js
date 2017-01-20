329bc8579a9f4e36cda575c51c3c9f03d9a981c0
Charlie Harris <ceharris92@gmail.com>
2017-01-20 21:57:48 +0000

    Use coordinates rather than ids to find adjacent blocks

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -35,14 +35,14 @@ export class BlockGrid {
             el.appendChild(colEl);
 
             for (let y = MAX_Y - 1; y >= 0; y--) {
-                let block = this.grid[x][y], // TODO: this is how we can reassign the blocks
+                let block = this.grid[x][y],
                     id = `block_${x}x${y}`,
                     blockEl = document.createElement('div');
 
                 blockEl.id = id;
                 blockEl.className = 'block';
                 blockEl.style.background = block.colour;
-                blockEl.addEventListener('click', (evt) => this.blockClicked(evt, block, id));
+                blockEl.addEventListener('click', (evt) => this.blockClicked(id));
                 colEl.appendChild(blockEl);
             }
         }
@@ -50,49 +50,41 @@ export class BlockGrid {
         return this;
     }
 
-    blockClicked (e, block, id) {
-        let adjacentCellsChecked = [];
-        var blockColour = document.getElementById(id).style.background;
+    blockClicked (targetBlockId) {
+        let blocksChecked = [];
+        const targetBlockColour = document.getElementById(targetBlockId).style.background;
 
-        function getIdsOfAdjacentCellsWithTheSameColour(elemId) { // ensure that current is inserted
-            // Make sure we haven't already run this check for an element.
-            if (adjacentCellsChecked.indexOf(elemId) >= 0) return null;
-            adjacentCellsChecked.push(elemId);
+        function connectedBlocksWithSameColour(blockId) {
+            const blockHasBeenChecked = blocksChecked.indexOf(blockId) >= 0;
 
-            var coordinates = elemId.substring(elemId.length - 3);
-            var xAxis = parseInt(coordinates.slice(0, 1));
-            var yAxis = parseInt(coordinates.slice(2, 3));
+            if (blockHasBeenChecked) return null;
+            blocksChecked.push(blockId);
 
-            var idOfBlockAbove = 'block_' + xAxis + 'x' + (yAxis - 1 );
-            var idOfBlockBelow = 'block_' + xAxis + 'x' + (yAxis + 1 );
-            var idOfBlockLeft = 'block_' + (xAxis - 1) + 'x' + yAxis;
-            var idOfBlockRight = 'block_' + (xAxis + 1) + 'x' + yAxis;
+            const block = document.getElementById(blockId).getBoundingClientRect();
 
-            var adjacentBlocks = [idOfBlockAbove, idOfBlockBelow, idOfBlockRight, idOfBlockLeft];
-            return adjacentBlocks.reduce(function(accumulator, id) {
-                const adjacentElement = document.getElementById(id);
+            const middleVertical = block.top + (block.height / 2);
+            const middleHorizontal = block.left + (block.width / 2);
 
-                if (!adjacentElement) return accumulator;
+            const blockBelow = document.elementFromPoint(middleHorizontal, block.bottom + 1);
+            const blockAbove = document.elementFromPoint(middleHorizontal, block.top - 1);
+            const blockLeft = document.elementFromPoint(block.left -1 , middleVertical);
+            const blockRight = document.elementFromPoint(block.right + 1, middleVertical);
 
-                if (adjacentElement.style.background === blockColour) {
-                    return [].concat(accumulator, getIdsOfAdjacentCellsWithTheSameColour(id));
+            const adjacentBlocks = [blockBelow, blockAbove, blockLeft, blockRight];
+
+            return adjacentBlocks.reduce((accumulator, adjacentBlock)  => {
+                if (!adjacentBlock) return accumulator;
+
+                if (adjacentBlock.style.background === targetBlockColour) {
+                    return [].concat(accumulator, connectedBlocksWithSameColour(adjacentBlock.id));
                 }
 
                 return accumulator;
-            }, elemId);
+            }, [ blockId ]);
         }
 
-        const idsToBeDeleted = [].concat(getIdsOfAdjacentCellsWithTheSameColour(id));
-        idsToBeDeleted.forEach(function(id) {
-            if (id) {
-                var elem = document.getElementById(id);
-                elem.remove();
-            }
-        });
-
-        //TODO: next step is to alter the IDs of the cells that have moved. Perhaps go through each column? Or find the nearest and don't rely on coords?
-
-        console.log(e, block, id);
+        connectedBlocksWithSameColour(targetBlockId)
+          .forEach((id) => (id ? document.getElementById(id).remove() : null))
     }
 }
 
