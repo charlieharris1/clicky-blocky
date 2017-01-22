249cc27cf4bb7cb761d5c1d038887655387361c5
Charlie Harris <ceharris92@gmail.com>
2017-01-22 20:21:08 +0000

    Improve test coverage

--- test/tests.js
+++ test/tests.js
@@ -4,7 +4,8 @@ import _ from 'lodash';
 import { Block, BlockGrid, COLOURS } from '../app/javascript/grid';
 
 const { describe, it, beforeEach } = window;
-
+const setBlueBackground = blocks => [].forEach.call(blocks, (block => block.setAttribute('style', 'background: blue')));
+const setRedBackground = blocks => [].forEach.call(blocks, (block => block.setAttribute('style', 'background: red')));
 
 beforeEach(() => {
   const element = document.getElementById('test');
@@ -31,48 +32,127 @@ describe('Block', () => {
 });
 
 describe('BlockGrid', () => {
-  it('should be created correctly', () => {
+  it('should create a grid when given the max number of blocks for x and y axes', () => {
     const X = 2;
     const Y = 2;
 
     const blockGrid = new BlockGrid(X, Y).render(document.querySelector('#test'));
+    assert.equal(blockGrid.MAX_X, 2, 'max x-axis value is 2');
+    assert.equal(blockGrid.MAX_Y, 2, 'max x-axis value is 2');
+
+    assert.equal(blockGrid.grid.length, 2, 'grid property is an array containing two elements');
+    assert.equal(blockGrid.grid[0].length, 2, 'grid property element is an array containing two blocks');
+
+    const block = blockGrid.grid[0][0];
+    assert.property(block, 'colour', 'block has colour property');
+    assert.property(block, 'x', 'block has x property');
+    assert.property(block, 'y', 'block has y property');
+  });
+});
+
+describe('BlockGrid render', () => {
+  it('should be created correctly', () => {
+    const X = 2;
+    const Y = 2;
+
+    new BlockGrid(X, Y).render(document.querySelector('#test'));
+
     const columnElements = document.getElementsByClassName('col');
+    const blockElements = document.getElementsByClassName('block');
 
     assert.equal(columnElements.length, 2, 'two columns created correctly');
-    assert.equal(document.getElementsByClassName('block').length, 4, 'four blocks created correctly');
+    assert.equal(blockElements.length, 4, 'four blocks created correctly');
 
     assert.equal(columnElements[0].childNodes.length, 2, 'column has two child nodes');
     assert.ok(columnElements[0].childNodes[0].className === 'block', 'col child nodes should be blocks');
     assert.ok(columnElements[0].childNodes[1].className === 'block', 'col child nodes should be blocks');
 
-    const blockColours = _.flatten(blockGrid.grid).map(block => block.colour);
-    assert.includeMembers(COLOURS, blockColours, 'all block colours are valid');
+    const blockElementColourIsAllowed = [].map.call(blockElements, block => _.includes(COLOURS, block.style.backgroundColor));
+    assert.notInclude(blockElementColourIsAllowed, false, 'all block elements should have valid colours');
+
+    const blockElementIds = [].map.call(blockElements, block => block.id);
+    assert.equal(_.uniq(blockElementIds).length, blockElements.length, 'all block element ids are unique');
   });
 });
 
-describe('blockClicked', () => {
-  it('should get rid of a square if there are none around it with the same colour', () => {
+
+describe('BlockGrid blockClicked', () => {
+  it('should remove only the target block if there are no blocks connected to it with the same colour', () => {
     const X = 2;
     const Y = 2;
-
-    const setBlocksToBlue = blocks => [].forEach.call(blocks, (block => block.setAttribute('style', 'color: blue')));
-    const setBlocksToRed = blocks => [].forEach.call(blocks, (block => block.setAttribute('style', 'color: red')));
 
     const blockGrid = new BlockGrid(X, Y);
     blockGrid.render(document.querySelector('#test'));
 
     const blocks = document.getElementsByClassName('block');
     const targetBlock = document.getElementById('block_1x0');
-    const adjacentBlocks = [].filter.call(blocks, (block => block.id !== targetBlock.id));
+    const remaniningBlocks = [].filter.call(blocks, (block => block.id !== targetBlock.id));
 
-    setBlocksToBlue(adjacentBlocks);
-    setBlocksToRed([targetBlock]);
+    setBlueBackground(remaniningBlocks);
+    setRedBackground([targetBlock]);
 
     assert.ok(document.getElementById(targetBlock.id), 'target element should exist before clicked');
 
     blockGrid.blockClicked(targetBlock.id);
 
     assert.isNotOk(document.getElementById(targetBlock.id), 'target element should be removed once clicked');
-    assert.equal(document.getElementsByClassName('block').length, 3, 'the three blue blocks should still exist');
+    assert.equal(document.getElementsByClassName('block').length, 3, 'the three blue blocks should be present');
+  });
+
+  it('should not remove a block of the same colour if it is not connected', () => {
+    const X = 2;
+    const Y = 2;
+
+    const blockGrid = new BlockGrid(X, Y);
+    blockGrid.render(document.querySelector('#test'));
+
+    const blocks = document.getElementsByClassName('block');
+    const targetBlock = document.getElementById('block_1x1');
+    const unconnectedBlock = document.getElementById('block_0x0');
+    const remainingBlocks = [].filter.call(blocks, (block => (block.id !== targetBlock.id) && (block.id !== unconnectedBlock.id)));
+
+    setBlueBackground(remainingBlocks);
+    setRedBackground([targetBlock, unconnectedBlock]);
+
+    blockGrid.blockClicked(targetBlock.id);
+
+    assert.isNotOk(document.getElementById(targetBlock.id), 'target element should be removed once clicked');
+    assert.ok(document.getElementById(unconnectedBlock.id), 'unconnected block with same colour as target should be present');
+    assert.equal(document.getElementsByClassName('block').length, 3, 'three blocks should be present');
+  });
+
+  it('should remove all connected blocks with the same colour as the target', () => {
+    const X = 3;
+    const Y = 3;
+
+    const blockGrid = new BlockGrid(X, Y);
+    blockGrid.render(document.querySelector('#test'));
+
+    const blocks = document.getElementsByClassName('block');
+
+    const targetBlock = document.getElementById('block_1x1');
+    const connectedBlockAbove = document.getElementById('block_1x0');
+    const connectedBlockBelow = document.getElementById('block_1x2');
+    const connectedBlockLeft = document.getElementById('block_0x1');
+    const connectedBlockRight = document.getElementById('block_2x1');
+
+    const unconnectedBlocks = [].filter.call(blocks,
+      (block =>
+        block.id !== targetBlock.id
+        && block.id !== connectedBlockAbove.id
+        && block.id !== connectedBlockBelow.id
+        && block.id !== connectedBlockLeft.id
+        && block.id !== connectedBlockRight.id));
+
+    setBlueBackground(unconnectedBlocks);
+    setRedBackground([targetBlock, connectedBlockAbove, connectedBlockBelow, connectedBlockLeft, connectedBlockRight]);
+
+    blockGrid.blockClicked(targetBlock.id);
+    assert.isNotOk(document.getElementById(targetBlock.id), 'target element should be removed once clicked');
+    assert.isNotOk(document.getElementById(connectedBlockAbove.id), 'connected block above with same colour should be removed');
+    assert.isNotOk(document.getElementById(connectedBlockBelow.id), 'connected block below with same colour should be removed');
+    assert.isNotOk(document.getElementById(connectedBlockLeft.id), 'connected block left with same colour should be removed');
+    assert.isNotOk(document.getElementById(connectedBlockRight.id), 'connected block right with same colour should be removed');
+    assert.equal(document.getElementsByClassName('block').length, 4, 'the four blue blocks should still exist');
   });
 });
