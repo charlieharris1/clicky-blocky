7136eeffe4149a805b7348cfe823c992d7ed37de
Charlie Harris <ceharris92@gmail.com>
2017-01-22 16:22:27 +0000

    Configure port to work on Heroku

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task("buildtests", function () {
 gulp.task('connect', function () {
     connect.server({
         livereload: true,
-        port: 9100
+        port: process.env.PORT || 9100
     });
 });
 
